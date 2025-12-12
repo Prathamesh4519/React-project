c5c2184a7f9eebc17af05252ed84c9dd896ea80e
Prathamesh4519 <prathamesh4519@gmail.com>
2025-12-12 20:12:02 +0530

    First project PasswordGenerator

--- Newproject/src/App.jsx
+++ Newproject/src/App.jsx
@@ -1,28 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import "tailwindcss";
+import { useCallback, useState, useEffect, useRef } from "react";
+import "./App.css";
 
 function App() {
-const [color,setColor]=useState("black")
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()~`";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
   return (
     <>
-    
-    <div className="w-full h-screen duration-200"  style={{backgroundColor:color}} >
-<div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-  <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl'>
-    <button onClick={()=>setColor("red")}
-    className='outline-none px-4 py-2 rounded-full text-white shadow-xl' style={{backgroundColor:"red"}}>Red</button>
-    <button onClick={()=>setColor("blue")}
-    className='outline-none px-4 py-2 rounded-full text-white shadow-xl' style={{backgroundColor:"blue"}}>Blue</button>
-    <button onClick={()=>setColor("green")}
-    className='outline-none px-4 py-2 rounded-full text-white shadow-xl' style={{backgroundColor:"green"}}>Green</button>
-  </div>
-</div>
-    </div>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 mt-30">
+        <h1 className=" text-lg font-bold 
+      bg-linear-to-r from-red-700 via-orange-500 to-yellow-300
+      bg-clip-text text-transparent text-center">Password generator</h1>
+
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPasswordToClipboard}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          >
+            copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => setLength(e.target.value)}
+            />
+            <label>Length: {length}</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => setNumberAllowed((prev) => !prev)}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => setCharAllowed((prev) => !prev)}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
